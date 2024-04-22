797511b7f8bd8364aa20e5fc20ccc3d14125b597
naimuddin94 <developernaimuddin@gmail.com>
2024-04-23 00:32:22 +0600

    update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,8 @@
 import { FaUserGraduate } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
 import { LuLayoutDashboard } from "react-icons/lu";
-import { MdOutlineWorkHistory, MdWorkHistory } from "react-icons/md";
+import { MdOutlineWorkHistory } from "react-icons/md";
 import { SlNotebook } from "react-icons/sl";
-import { VscGraphLine } from "react-icons/vsc";
 import Swal from "sweetalert2";
 
 export const navigation = [
@@ -39,58 +38,6 @@ export const navigation = [
     icon: { name: IoSettingsOutline, size: 22 },
     child: ["Business Settings", "SMS Settings"],
   },
-];
-
-export const customerTabs = [
-  { label: "Project", icon: { name: MdWorkHistory, size: 24 } },
-  // { label: "Ledger", icon: { name: PiNotebookFill, size: 24 } },
-  // { label: "Sales", icon: { name: HiMiniShoppingBag, size: 24 } },
-  { label: "Documents & Notes", icon: { name: SlNotebook, size: 20 } },
-  // { label: "Payment", icon: { name: MdPayments, size: 24 } },
-  { label: "Tasks", icon: { name: VscGraphLine, size: 22 } },
-];
-
-export const permissions = [
-  {
-    name: "Role Management",
-    permissionsGroup: ["Add Role", "Update Role", "Delete Role"],
-  },
-  {
-    name: "Biodata",
-    permissionsGroup: ["Add Biodata", "Update Biodata", "Delete Biodata"],
-  },
-  {
-    name: "Packages",
-    permissionsGroup: ["Add Package", "Update Package", "Delete Package"],
-  },
-  {
-    name: "Subscriptions",
-    permissionsGroup: [
-      "Add Subscription",
-      "Update Subscription",
-      "Delete Subscription",
-    ],
-  },
-  {
-    name: "Restaurants",
-    permissionsGroup: [
-      "Add Restaurant",
-      "Update Restaurant",
-      "Delete Restaurant",
-    ],
-  },
-  {
-    name: "Kazi Office",
-    permissionsGroup: ["Add Office", "Update Office", "Delete Office"],
-  },
-];
-
-export const banks = [
-  "AB Bank",
-  "Dutch Bangla Bank",
-  "City Bank",
-  "Islami Bank",
-  "One Bank",
 ];
 
 export const convertToSlug = (text: string, replaceWith = "-") => {
