e8d2e813a945e8d5d65f96cf35a2a2fbd5830b51
naimuddin94 <developernaimuddin@gmail.com>
2024-04-23 22:53:28 +0600

    update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Card from "../components/shared/Card";
+import { useGetUsersQuery } from "../redux/features/userApi";
 
 const Home = () => {
+  const { data } = useGetUsersQuery("Users");
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-      <Card title="Total Users" value={100} />
+      <Card title="Total Users" value={data?.data?.length || 0} />
       <Card title="Total Profiles" value={100} />
       <Card title="Total Company" value={100} />
     </div>
