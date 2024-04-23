7367dd2957386e04518b2da645aa13009e45f39b
naimuddin94 <developernaimuddin@gmail.com>
2024-04-24 00:42:14 +0600

    update baseApi.ts

--- src/redux/features/baseApi.ts
+++ src/redux/features/baseApi.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // https://eprofile-api.vercel.app
-// http://localhost:5000/api/v1
+// http://localhost:5000
 // Define a service using a base URL and expected endpoints
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://eprofile-api.vercel.app/api/v1",
+    baseUrl: "http://localhost:5000/api/v1",
     credentials: "include",
   }),
   tagTypes: ["User", "Company", "Profile"],
