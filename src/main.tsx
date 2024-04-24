f23ede333d76acf5f9ab4ccb9789af66bf722144
naimuddin94 <developernaimuddin@gmail.com>
2024-04-25 01:09:09 +0600

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,30 @@
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import "./css/style.css";
-import "./css/satoshi.css";
-import router from "./Routes/Route";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "react-auth-kit";
+import createStore from "react-auth-kit/createStore";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Route";
+import "./css/satoshi.css";
+import "./css/style.css";
 import store from "./redux/store/store";
+
+const authStore = createStore({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === "https:",
+});
 
 // Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </Provider>
+  <AuthProvider store={authStore}>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
+  </AuthProvider>
 );
